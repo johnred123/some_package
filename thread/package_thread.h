96ca65b565346095056daf943273da7053717cb8
johnred123 <john_red@sohu.com>
2015-03-15 21:38:06 +0800

    add package_thread.h

---
+++ thread/package_thread.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ 
+  File Name     : package_thread.h
+  Version       : Initial Draft
+  Author        : john kazmath (johnred123@github)
+  Created       : 
+  Last Modified :
+  Function List : package_pthread_init
+                  package_pthread_suspend
+                  package_pthread_resume
+  History       :
+  1.Date        : 
+    Author      : john kazmath (johnred123@github)
+    Modification: Created file
+
+******************************************************************************/
+#ifndef _PACKAGE_THREAD_H_
+#define _PACKAGE_THREAD_H_
+
+void package_pthread_init(void);
+void package_pthread_suspend(void);
+void package_pthread_resume(void);
+
+#endif /* endif for file package_thread.h */
