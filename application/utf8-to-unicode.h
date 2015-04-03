00b1611e2baaee80b8da5ce626cf4ae920ddd635
johnred123 <john_red@sohu.com>
2015-04-03 15:06:57 +0800

    add conver func

--- application/utf8-to-unicode.h
+++ application/utf8-to-unicode.h
@@ -27,10 +27,19 @@
 
 #include "../malloc/mem_malloc.h"
 
-
 #define _UTF8_TO_UNICODE_H_
 
 #define MAXSIZE 140
+
+#define IS_AF(c)  ((c >= 'A') && (c <= 'F'))
+#define IS_af(c)  ((c >= 'a') && (c <= 'f'))
+#define IS_09(c)  ((c >= '0') && (c <= '9'))
+#define ISVALIDHEX(c)  IS_AF(c) || IS_af(c) || IS_09(c)
+#define ISVALIDDEC(c)  IS_09(c)
+#define CONVERTDEC(c)  (c - '0')
+
+#define CONVERTHEX_alpha(c)  (IS_AF(c) ? (c - 'A'+10) : (c - 'a'+10))
+#define CONVERTHEX(c)   (IS_09(c) ? (c - '0') : CONVERTHEX_alpha(c))
 
 void utf8_to_unicode ( unsigned char *cpInput , int Len );
 
